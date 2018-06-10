a9d3ef6d6c7f7fa5044822d233237ee049bb7dff
pestretsov <pestretsov@me.com>
2018-06-10 15:50:04 +0300

    cleaned code

--- page_url_trigger.js
+++ page_url_trigger.js
@@ -3,10 +3,10 @@ function notification_trigger(urls, details) {
 
     const res = urls.filter(url => details.url.includes(url));
     if (res.length > 0) {
-        alert("HOW DARE YOU!")
+        alert("HOW DARE YOU!");
     }
 };
 
-const urls = ["youtube.com/"] // shouldn't be hardcoded -- mb get it from user's settings?
+const urls = ["youtube.com/"]; // shouldn't be hardcoded -- mb get it from user's settings?
 
 chrome.webNavigation.onCompleted.addListener(details => notification_trigger(urls, details));
