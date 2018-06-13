e11e7d9497a8463f9db01098c934f17c2ff5254c
pestretsov <pestretsov@me.com>
2018-06-13 23:04:55 +0300

    added whitelist and better filtering

--- page_url_trigger.js
+++ page_url_trigger.js
@@ -1,12 +1,24 @@
-function notification_trigger(blacklist_urls, details) {
-    const parsed_url = parse_url(details.url);
-    const domain = parsed_url.domain;
-    
-    if (blacklist_urls.includes(domain)) {
+function is_url_in_list(url, list) {
+    log(list);
+    return list.filter(u => u.domain === url.domain && u.category === url.category).length > 0;
+};
+
+function notification_trigger(blacklist, whitelist, url) {
+    if (is_url_in_list(url, whitelist)) {
+        return;
+    }
+
+    if (is_url_in_list(url, blacklist)) {
         alert("HOW DARE YOU!");
     }
 };
 
-const blacklist_urls = ["youtube.com", "kinovod.net"]; // shouldn't be hardcoded -- mb get it from user's settings?
+const blacklist = ["youtube.com", "kinovod.net"]; // shouldn't be hardcoded -- mb get it from user's settings?
+const whitelist = ["youtube.com/embed"]; 
 
-chrome.webNavigation.onCompleted.addListener(details => notification_trigger(blacklist_urls, details));
+const parsed_blacklist = blacklist.map(url => parse_url(url));
+const parsed_whitelist = whitelist.map(url => parse_url(url));
+
+chrome.webNavigation.onCompleted.addListener(
+    details => notification_trigger(parsed_blacklist, parsed_whitelist, parse_url(details.url))
+);
