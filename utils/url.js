32b234b91c621d01f306f4488d3dd931acf9e6e0
pestretsov <pestretsov@me.com>
2018-06-13 17:00:43 +0300

    improved regex

--- utils/url.js
+++ utils/url.js
@@ -1,12 +1,17 @@
-const url_regex = RegExp("^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)");
+const url_regex = RegExp("^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)\/?([^:\/\n]+)?");
 
 function get_domain(url) {
     return url_regex.exec(url)[1];
 }
 
+function get_category(url) {
+    return url_regex.exec(url)[2];
+}
+
 function parse_url(url) {
     return {
         domain: get_domain(url),
+        category: get_category(url),
         full: url
     };
 }
